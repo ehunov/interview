6a0fc0bd470c6a109f0ba3361cc1e30de029c7ce
Ehunov <anton.ehunov@lamoda.ru>
2022-07-19 22:17:45 +0400

    phpunit support added

---
+++ src/leetcode/data_structures/array/merge_sorted_test.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+namespace Ehunov\Interview\leetcode\data_structures\array;
+
+use PHPUnit\Framework\TestCase;
+
+final class merge_sorted_test extends TestCase
+{
+    /**
+     * @dataProvider provideTestMergeData
+     */
+    public function testMerge(array $nums1, int $n, array $nums2, int $m, array $expected): void
+    {
+        $this->assertEquals($expected, merge_sorted::merge($nums1, $n, $nums2, $m));
+    }
+
+    public function provideTestMergeData(): iterable
+    {
+        yield [
+            [1, 2, 3, 0, 0, 0],
+            3,
+            [2, 5, 6],
+            3,
+            [1, 2, 2, 3, 5, 6],
+        ];
+        yield [
+            [1],
+            1,
+            [],
+            0,
+            [1],
+        ];
+        yield [
+            [0],
+            0,
+            [1],
+            1,
+            [1],
+        ];
+        yield [
+            [2, 0],
+            1,
+            [1],
+            1,
+            [1, 2],
+        ];
+    }
+}
